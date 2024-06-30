583064b283f7d7d9d244e8037d22928f867feb4e
Ankita Trap <smpratik888@gmail.com>
2024-06-30 21:37:20 +0530

    user_input

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+});
   return (
+    <> 
+    {/* because multiple jsx are needed to be rendered so fragments are needed(<> a,b,c</>) */}
     <Header />
+    <UserInput onChange={handleChange}/>
+    </>
   )
 }
 
