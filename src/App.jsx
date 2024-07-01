4e6145344f0b885fda0aa380b8d881eec767c882
Ankita Trap <smpratik888@gmail.com>
2024-07-01 13:54:30 +0530

    result-addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header.jsx"
+import Results from "./components/Results.jsx";
 import UserInput from "./components/UserInput.jsx"
 import { useState } from "react";
 
@@ -9,11 +10,20 @@ function App() {
     expectedReturn: 6,
     duration: 10,
 });
+function handleChange(inputIdentifier, newValue){
+  setUserInput(prevUserInput => {
+      return {
+          ...prevUserInput,
+          [inputIdentifier]: newValue,
+  };
+});
+}
   return (
     <> 
     {/* because multiple jsx are needed to be rendered so fragments are needed(<> a,b,c</>) */}
     <Header />
-    <UserInput onChange={handleChange}/>
+    <UserInput userInput={userInput} onChange={handleChange}/>
+    <Results input={userInput}/>
     </>
   )
 }
